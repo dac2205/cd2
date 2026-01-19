b51e51dbeb2a55bbfcceea107a804cc3f8cc0bfa
Dac Nguyen <dac2205@gmail.com>
2026-01-19 13:27:31 +0700

    fix: use robust redirect url logic pointing to root

--- lib/auth.tsx
+++ lib/auth.tsx
@@ -79,7 +79,7 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
         const { error } = await supabase.auth.signInWithOAuth({
             provider: 'google',
             options: {
-                redirectTo: `${window.location.origin}/auth/callback`,
+                redirectTo: `${window.location.protocol}//${window.location.host}/`,
             }
         });
         if (error) {
